a0856afe21169f299927c92f769c3f377ea623ae
muhammadfikri19 <200705048@student.ar-raniry.ac.id>
2023-05-30 00:38:29 +0700

    first commit

---
+++ lib/Widgets/Book Widgets/circular_image.dart
@@ -0,0 +1,64 @@
+import 'package:cozy_application/Pages/home_page.dart';
+import 'package:flutter/material.dart';
+// import 'package:cozy_application/Pages/home_page.dart';
+import 'package:cozy_application/Pages/splash_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CircularImage extends StatelessWidget {
+  const CircularImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 70),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(10),
+              topLeft: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            child: Image.asset(
+              'assets/images/fikri1.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Text(
+            'M.fikriansyah',
+            style: GoogleFonts.poppins(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Text(
+            "17 : 21",
+            style: GoogleFonts.poppins(
+              fontSize: 10,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(19),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(),
+                ),
+              );
+            },
+            child: const Text('Back to Home Page'),
+          ),
+        )
+      ],
+    );
+  }
+}
